a9917834069699f9ec99670e6f696aae7dde567f
Enver Eren Güçlü <envererenguclu@gmail.com>
2026-05-13 01:32:57 +0300

    railway start fix

--- server.js
+++ server.js
@@ -226,5 +226,5 @@ io.on("connection", (socket) => {
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Sunucu ${PORT} portunda çalışıyor`);
+  console.log("Sunucu çalışıyor");
 });
